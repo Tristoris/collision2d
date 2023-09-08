80a4ae807b4264afbe3381888c9c00be488fd09c
Alexander Yurkov <alexander.v.yurkov@gmail.com>
2023-09-08 15:51:31 +0200

    first commit? ig lmao

---
+++ Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collision2D
+{
+    public class Triangle
+    {
+
+        public Triangle() {
+            
+        }
+
+        public bool isColliding (Triangle triangle)
+        {
+            return true;
+        }
+    }
+}
